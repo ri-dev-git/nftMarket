0335214a88233f386694e8c2667f4497beff4a40
ri-dev-git <rishi9850@outloo.com>
2023-02-07 15:31:02 +0530

    FIX

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,19 @@ import '@/styles/globals.css'
 import Head from 'next/head'
 import Header from '../components/header.js'
 import '@rainbow-me/rainbowkit/styles.css';
+import {useEffect, Suspense  ,useState} from 'react';
 import {
   getDefaultWallets,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { configureChains, createClient, WagmiConfig,useAccount, useConnect } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum,goerli } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 
 export default function App({ Component, pageProps }) {
+
   const { chains, provider } = configureChains(
     [mainnet, polygon, optimism, arbitrum,goerli],
     [
@@ -30,7 +32,16 @@ export default function App({ Component, pageProps }) {
     connectors,
     provider
   })
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => {
+      setHydrated(true)
+    
+      return () => {
+        
+      }
+    }, [])
   return (
+   hydrated?
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <Head>
@@ -39,7 +50,7 @@ export default function App({ Component, pageProps }) {
         <Header/>
         <Component {...pageProps} />
       </RainbowKitProvider>
-    </WagmiConfig>
+    </WagmiConfig>:"Bleh"
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 
 // import styles from '@/styles/Home.module.css'
-
+import {useAccount, useConnect } from 'wagmi';
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { connector: activeConnector, isConnected } = useAccount()
+  // const { connect, error, isLoading, pendingConnector } =useConnect()
+  
+  console.log(isConnected)
   return (
- <div>
-  Hello
- </div>
+ isConnected?<div>
+  <div>
+    
+  </div>
+ </div>:"Please Connect Wallet"
   )
 }
